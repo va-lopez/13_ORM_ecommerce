150907b179e39cfbb293aa0479219c728ceac50a
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-11 19:01:40 -0500

    setting up the models

---
+++ models/Category.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+
+//making sure your connected to the database
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+//this is how you initialize the database
+//whenever we initialize the model, we are giving it the columns for the tables itself
+//but we also have these options for the models themselves. The "Sequelize" section
+//that we are using sequealize as the database, timestaps are optional if we wanted
+//to include it in the  table.
+//freezeTableName -> prevents it from being categories, making it plural, right now its singular
+//underscored -> if you have multiple words in a name it includes the underscores
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
+    category name: {
+      type: DataTypes.STRING,
+      allowNull:false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
