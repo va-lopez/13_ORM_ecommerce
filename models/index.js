f507890a7025d78ff1af9613796ec77507dbd1a9
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-11 20:12:05 -0500

    added category routes

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 //when we import models, it has a relationship method like
 //"belongs to", amounts to "reference rows"
-Product.belongsto(Category, {
+Product.belongsTo(Category, {
   foreignKey:'category_id',
   //other options include onDelete:which means you can choose
   //what happens to the project. If "Cascade" then deletes, if "null" then it sets the foreign key to null. 
@@ -19,14 +19,14 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   //you can add the "as: " to set an alias
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
 })
 
