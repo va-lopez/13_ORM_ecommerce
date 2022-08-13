ced6e992e25c36f6320ba4e6d8fc84f0d0aee389
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-13 15:59:39 -0500

    created readme file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 13_ORM_ecommerce
+
+## Description: 
+This application demonstrates the backend of an e-commerce website. It uses Express.js API and a MySQL database. 
+
+## Installation
+Make sure to install all your dependencies including Node, Inquirer, Dotenv, mysql2, sequelize. 
+
+1. Open the terminal in the root directory and run the command "npm i". 
+
+2. Run MySql to source the schema. In the terminal, type in the command "mysql -u root -p". Enter in your credentials and once successfully logged in, run the command "source db/schema.sql". 
+
+3.Once the database has been created, type in "Quit" to get out of mysql.
+
+4. In the terminal type in the command "pm run seed" so as to seed all the data into the database.
+
+5. Lastly, run "node server.js"
+
+## Usage
+link to github project page: https://github.com/va-lopez/13_ORM_ecommerce
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({  
+    include: [
+        Category,{model:Tag,through: ProductTag}],
+    })
+  .then((data) => res.json(data))
+  .catch((err) => res.status(500).json(err))
+})
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category,{model:Tag,through: ProductTag}],
+})
+  .then((data) => res.json(data))
+  .catch((err) => res.status(500).json(err))
+})
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +106,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-});
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((data) => res.json(data))
+  .catch((err) => res.status(500).json(err))
+})
 
 module.exports = router;
